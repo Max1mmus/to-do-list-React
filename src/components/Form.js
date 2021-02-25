372dd0f966ae1f493c7b86f4c329a847b03be52a
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2021-02-25 20:09:42 +0100

    props destructuring

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export const Form = (props) => {
+export const Form = ({onSubmit, onChange, inputValue}) => {
     return(
         <div className='inputField'>
-            <form onSubmit={props.onSubmit}>
+            <form onSubmit={onSubmit}>
                 <h1>To-do List</h1>
                 <input 
                     type='text'
                     placeholder='Task...'
-                    value={props.value}
-                    onChange={props.onChange}>
+                    value={inputValue}
+                    onChange={onChange}>
                 </input>
                 <button type='submit' className='submitBtn'> Add task </button>
             </form>
