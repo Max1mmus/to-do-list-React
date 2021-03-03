5f9d713cabf8e0b417fabc84b338d9911e6ad103
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2021-03-03 22:11:17 +0100

    delete div wrapper

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,17 +2,14 @@ import React from "react";
 
 export const Form = ({onSubmit, onChange, inputValue}) => {
     return(
-        <div className="form-wrapper">
-            <form onSubmit={onSubmit}>
-                <h1>To-do List</h1>
-                <input 
-                    type="text"
-                    placeholder="Task..."
-                    value={inputValue}
-                    onChange={onChange}>
-                </input>
-                <button type="submit" className="submitBtn"> Add task </button>
-            </form>
-        </div>
+        <form onSubmit={onSubmit}>
+            <input 
+                type="text"
+                placeholder="Task..."
+                value={inputValue}
+                onChange={onChange}>
+            </input>
+            <button type="submit" className="submitBtn"> Add task </button>
+        </form>
     )
 }
