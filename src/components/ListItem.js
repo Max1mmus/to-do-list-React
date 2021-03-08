959ce46cb94c9c818e9ec462f2a3cc53b263ffac
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2021-03-08 20:25:21 +0100

    rename list to ListItem

---
+++ src/components/ListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const ListItem = ({onDelete, checkboxChange, index, task}) => {
+    return(
+        <li style={{ backgroundColor: task.isDone ? "#8EFFEF57" : null }}>
+            <input 
+                type="checkbox" 
+                id={`input-${index}`} 
+                onChange={checkboxChange}
+                checked={task.isDone}>
+            </input>
+            <div className="card-content">
+                <p className="task-text">{task.taskContent}</p>
+                <p className="timestamp">{task.timeStamp}</p>
+            </div>
+            <button className="deleteBtn" onClick={onDelete}>
+                Delete
+            </button>
+        </li>   
+    )
+}
