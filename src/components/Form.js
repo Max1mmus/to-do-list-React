7af7afc00cbb966f69ff63c780d0f3908bd7b486
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2021-03-10 02:25:31 +0100

    add handleSubmit + handleChange func & newTask state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const Form = ({onSubmit, onChange, inputValue}) => {
+export const Form = ({addTask}) => {
+    const [newTask, setNewTask] = useState("");
+    
+    function handleSubmit (e) {
+        e.preventDefault();
+        if (newTask === "") return;
+        addTask(newTask);
+        setNewTask("");
+    }
+
+    function handleChange(e) {
+        setNewTask(e.target.value);
+    }
+
     return(
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit}>
             <input 
                 type="text"
                 placeholder="Task..."
-                value={inputValue}
-                onChange={onChange}>
+                value={newTask}
+                onChange={handleChange}>
             </input>
             <button type="submit" className="submitBtn"> Add task </button>
         </form>
